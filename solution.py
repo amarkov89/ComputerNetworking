c921fd8e22521f42c5fe4e772aaff01c6e32c828
Alan Markov <markov.alan@gmail.com>
2021-09-23 13:00:16 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ def webServer(port=13331):
   while True:
     #Establish the connection
     #print('Ready to serve...')
-	#print ("Ready to serve...")
+	print ("Ready to serve...")
     connectionSocket, addr = serverSocket.accept()
     try:
 
@@ -22,7 +22,7 @@ def webServer(port=13331):
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.read()
-		#print (outputdata)
+		print (outputdata)
         
         #Send one HTTP header line into socket.
         #Fill in start
