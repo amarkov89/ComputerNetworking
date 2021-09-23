1648ccd869e560dfe8fc0bf6435a9f2b86c1f6fb
Alan Markov <markov.alan@gmail.com>
2021-09-23 12:42:21 -0400

    comitt

--- solution.py
+++ solution.py
@@ -1,34 +1,63 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
 
-# welcome_assignment_answers
-# Input - All eight questions given in the assignment.
-# Output - The right answer for the specific question.
 
-def welcome_assignment_answers(question):
-    if question ==  "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
-        return (answer)
-    elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-        return (answer)
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No" 
-        return (answer)
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes" 
-        return (answer)
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-        return (answer)
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-        return (answer)
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-        return (answer)
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = int(5)
-        return (answer)
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = int(4)
-        return(answer)
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  serverSocket.listen(1)
+  
+  while True:
+    #Establish the connection
+    #print('Ready to serve...')
+	print ("Ready to serve...")
+    connectionSocket, addr = serverSocket.accept()
+    try:
 
+      try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+		print (outputdata)
+        
+        #Send one HTTP header line into socket.
+        #Fill in start
+		
+		connectionSocket.send("\nHTTP/1.1 200 OK\n\n".encode())
+
+        #Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        #Fill in start
+		
+		connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
+
+        #Fill in end
+
+
+        #Close client socket
+        #Fill in start
+		
+		connectionSocket.close()
+
+        #Fill in end
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
